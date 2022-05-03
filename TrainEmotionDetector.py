731407f1bae8e40d60bacdd7a8100f0d1736c962
Haluk Can Sarıöz <halukcansarioz19@gmail.com>
2022-05-04 00:29:22 +0300

    Emotion Detection
    Artificial Intelligence and Image Based Face Expression Recognition System

---
+++ TrainEmotionDetector.py
@@ -0,0 +1,113 @@
+import cv2
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+import tensorflow as tf
+from tensorflow.python.keras.layers import  Dense,Adam
+from tensorflow.keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+
+train_data_gen = ImageDataGenerator(rescale=1./255)
+validation_data_gen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_data_gen.flow_from_directory(
+        'data/train',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = validation_data_gen.flow_from_directory(
+        'data/test',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+
+emotion_model_info = emotion_model.fit_generator(
+        train_generator,
+        steps_per_epoch=28709 // 64,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=7178 // 64)
+
+model_json = emotion_model.to_json()
+with open("emotion_model.json", "w") as json_file:
+    json_file.write(model_json)
+
+emotion_model.save_weights('../Emotion_detection_with_CNN-main/model/emotion_model.h5')
+
+loss = emotion_model_info.history['loss']
+val_loss = emotion_model_info.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = emotion_model_info.history['accuracy']
+val_acc = emotion_model_info.history['val_accuracy']
+
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+from keras.models import load_model
+
+my_model = load_model('emotion_detection_model_100epochs.h5', compile=False)
+
+test_img, test_lbl = validation_generator.__next__()
+predictions=my_model.predict(test_img)
+
+predictions = np.argmax(predictions, axis=1)
+test_labels = np.argmax(test_lbl, axis=1)
+
+from sklearn import metrics
+print ("Accuracy = ", metrics.accuracy_score(test_labels, predictions))
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(test_labels, predictions)
+import seaborn as sns
+sns.heatmap(cm, annot=True)
+
+class_labels=['Angry','Disgust', 'Fear', 'Happy','Neutral','Sad','Surprise']
+n=random.randint(0, test_img.shape[0] - 1)
+image = test_img[n]
+orig_labl = class_labels[test_labels[n]]
+pred_labl = class_labels[predictions[n]]
+plt.imshow(image[:,:,0], cmap='gray')
+plt.title("Original label is:"+orig_labl+" Predicted is: "+ pred_labl)
+plt.show()
+
